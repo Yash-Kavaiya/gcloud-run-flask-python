123416fb7701588a75c0680c70cf22123be57302
Yash Kavaiya <yash.kavaiya3@gmail.com>
2024-05-14 19:19:11 +0000

    Consider adding a step for automated versioning

--- setup.py
+++ setup.py
@@ -2,17 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='gcloud-run-flask-python',
-    version='0.1.0',
+    version='0.1',
     packages=find_packages(),
-    include_package_data=True,
     install_requires=[
         'Flask==3.0.3',
         'gunicorn==22.0.0',
-        'Werkzeug==3.0.3',
+        'Werkzeug==3.0.3'
     ],
     entry_points={
         'console_scripts': [
-            'gcloud-run-flask-python=your_package.main:main',
-        ],
-    },
+            'gcloud-run-flask-python = gcloud_run_flask_python.cli:main'
+        ]
+    }
 )
